9c1bdf6cef95752af7118df7db7a81d52f9a8c69
thuytn <thuy140583@gmail.com>
2018-06-30 21:18:10 +0700

    test for_loop

---
+++ For_loop.js
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+	<title> While loo </title>
+	<script type="text/javascript">
+
+	document.write("<br> Using for While loop <br/>");
+	var i=0, j=1, k;
+	document.write("fibonaci toi 40 <br/>")
+	while (1<40) {
+		document.write(i+ "  ")
+		k=i+j;
+		i=j;
+		j=k;
+	}
+	</script> 
+<body>
+</body>
+</html>
